8815c6c8621f7879f774b9dc7556d2a7b1e9b67d
Tejash kharote <tejashrocks1@gmail.com>
2022-11-16 23:13:11 -0800

    Update user.php

--- crud/user.php
+++ crud/user.php
@@ -33,7 +33,7 @@ if (isset($_POST['submit'])){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>crud operation</title>
+    <title>operation</title>
   </head>
   <body>
   <div class="container my-5">
@@ -44,19 +44,10 @@ if (isset($_POST['submit'])){
   </div>
 
   <div class="form-group">
-    <label for="exampleInputEmail1">Email</label>
-    <input type="email" name="email"class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <label for="exampleInputEmail1">price</label>
+    <input type="text" name="email"class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
   </div>
 
-  <div class="form-group">
-    <label for="exampleInputEmail1">contact</label>
-    <input type="text" name="contact" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter mobile No">
-</div>
-
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password"class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
 
   <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 </form>
